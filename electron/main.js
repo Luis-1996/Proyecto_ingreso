591a9504a8f6bb359f0c7e0f088bc4df69ddce1c
Luis-1996 <luiscarlossibaja18@gmail.com>
2026-07-08 16:41:41 -0500

    fix: disable webSecurity in Electron to bypass CORS + rebuild backend.exe with CORS fixes

--- electron/main.js
+++ electron/main.js
@@ -212,6 +212,7 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
       nodeIntegration: false,
+      webSecurity: false,
     },
   })
 
